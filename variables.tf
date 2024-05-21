b02ed25cf43fcbf956089b1b1cf2ffdc576bbaac
楊沅智 <allen@yangyuanzhidebijixingdiannao.local>
2024-05-21 10:59:53 +0800

    add vars

--- variables.tf
+++ variables.tf
@@ -16,3 +16,14 @@ variable "instance_name" {
   default     = "Provisioned by Terraform"
 }
 
+variable "tfc_aws_dynamic_credentials" {
+  description = "Object containing AWS dynamic credentials configuration"
+  type = object({
+    default = object({
+      shared_config_file = string
+    })
+    aliases = map(object({
+      shared_config_file = string
+    }))
+  })
+}
